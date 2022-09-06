f2830de3aa5b182b18bbdcbb2e2ac8c8b44282d9
Joel Anaya <bomijoe@gmail.com>
2022-09-06 18:51:40 -0500

    dia 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'y quien lo decidio';
+const nuevaString = 'dia 2 que show';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 6;
